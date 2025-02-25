92f2524052df6779ee6607825bc951b19e0db3eb
Gudfit <daniel2468114@gmail.com>
2025-02-25 20:22:56 +0000

    Transpose kernal take 2

--- cudasrc/src/CUDASol.cu
+++ cudasrc/src/CUDASol.cu
@@ -9,11 +9,29 @@
 #define TILE_WIDTH_PADDED (TILE_WIDTH + 1)
 
 __global__ void transpose_kernel(const float *B, float *BT, int k, int n) {
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  int j = blockIdx.y * blockDim.y + threadIdx.y;
+  __shared__ float block[BLOCK_DIM][BLOCK_DIM + 1];
 
-  if (i < k && j < n)
-    BT[j * k + i] = B[i * n + j];
+  int x = blockIdx.x * BLOCK_DIM + threadIdx.x;
+  int y = blockIdx.y * BLOCK_DIM + threadIdx.y;
+
+  if (x < k && y < n) {
+    unsigned int index_in = x * n + y;
+    block[threadIdx.y][threadIdx.x] = B[index_in];
+  }
+
+  /* Ensure all threads have written to shared memory */
+  __syncthreads();
+
+  /* Transpose indices for writing back */
+  /* Swap blockIdx.x and blockIdx.y */
+  x = blockIdx.y * BLOCK_DIM + threadIdx.x;
+  y = blockIdx.x * BLOCK_DIM + threadIdx.y;
+
+  /* Write the transposed data back to global memory */
+  if (x < n && y < k) {
+    unsigned int index_out = y * n + x;
+    BT[index_out] = block[threadIdx.x][threadIdx.y];
+  }
 }
 
 __global__ void matmul_kernel_naive(const float *A, const float *BT, float *C,
