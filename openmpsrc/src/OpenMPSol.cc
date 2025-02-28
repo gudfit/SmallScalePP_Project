471548329de89fcfece7ac5abf3e0395c4c63440
Gudfit <daniel2468114@gmail.com>
2025-02-28 17:19:09 +0000

    clean uo

--- openmpsrc/src/OpenMPSol.cc
+++ openmpsrc/src/OpenMPSol.cc
@@ -52,6 +52,7 @@ public:
   };
 };
 
+// --------------------- TRANSPOSE ------------------------- //
 /*
  * transpose
  *
@@ -61,6 +62,7 @@ public:
  * sizes n,k
  * @return void
  */
+
 void transpose(const float *B, float *BT, int k, int n) {
   if (k <= 0 || n <= 0)
     return;
@@ -80,6 +82,80 @@ void transpose(const float *B, float *BT, int k, int n) {
     }
   }
 }
+
+// or bottom one is justt a tiny bit faster than top
+
+/*
+void transpose(const float *__restrict src, float *__restrict dst, int n,
+                 int k) {
+  const int TILE = 32;
+#pragma omp parallel for collapse(2)
+  for (int i = 0; i < n; i += TILE) {
+    for (int j = 0; j < k; j += TILE) {
+      int i_max = std::min(n, i + TILE);
+      int j_max = std::min(k, j + TILE);
+      // Transpose the tile [i..i_max) x [j..j_max)
+      for (int ii = i; ii + 7 < i_max; ii += 8) {
+        for (int jj = j; jj + 7 < j_max; jj += 8) {
+          // Use the unaligned load instruction
+          __m256 row0 = _mm256_loadu_ps(&src[ii * k + jj]);
+          __m256 row1 = _mm256_loadu_ps(&src[(ii + 1) * k + jj]);
+          __m256 row2 = _mm256_loadu_ps(&src[(ii + 2) * k + jj]);
+          __m256 row3 = _mm256_loadu_ps(&src[(ii + 3) * k + jj]);
+          __m256 row4 = _mm256_loadu_ps(&src[(ii + 4) * k + jj]);
+          __m256 row5 = _mm256_loadu_ps(&src[(ii + 5) * k + jj]);
+          __m256 row6 = _mm256_loadu_ps(&src[(ii + 6) * k + jj]);
+          __m256 row7 = _mm256_loadu_ps(&src[(ii + 7) * k + jj]);
+          __m256 t0 = _mm256_unpacklo_ps(row0, row1);
+          __m256 t1 = _mm256_unpackhi_ps(row0, row1);
+          __m256 t2 = _mm256_unpacklo_ps(row2, row3);
+          __m256 t3 = _mm256_unpackhi_ps(row2, row3);
+          __m256 t4 = _mm256_unpacklo_ps(row4, row5);
+          __m256 t5 = _mm256_unpackhi_ps(row4, row5);
+          __m256 t6 = _mm256_unpacklo_ps(row6, row7);
+          __m256 t7 = _mm256_unpackhi_ps(row6, row7);
+          __m256 tt0 = _mm256_shuffle_ps(t0, t2, _MM_SHUFFLE(1, 0, 1, 0));
+          __m256 tt1 = _mm256_shuffle_ps(t0, t2, _MM_SHUFFLE(3, 2, 3, 2));
+          __m256 tt2 = _mm256_shuffle_ps(t1, t3, _MM_SHUFFLE(1, 0, 1, 0));
+          __m256 tt3 = _mm256_shuffle_ps(t1, t3, _MM_SHUFFLE(3, 2, 3, 2));
+          __m256 tt4 = _mm256_shuffle_ps(t4, t6, _MM_SHUFFLE(1, 0, 1, 0));
+          __m256 tt5 = _mm256_shuffle_ps(t4, t6, _MM_SHUFFLE(3, 2, 3, 2));
+          __m256 tt6 = _mm256_shuffle_ps(t5, t7, _MM_SHUFFLE(1, 0, 1, 0));
+          __m256 tt7 = _mm256_shuffle_ps(t5, t7, _MM_SHUFFLE(3, 2, 3, 2));
+          // Blend results to form transposed rows
+          row0 = _mm256_permute2f128_ps(tt0, tt4, 0x20);
+          row1 = _mm256_permute2f128_ps(tt1, tt5, 0x20);
+          row2 = _mm256_permute2f128_ps(tt2, tt6, 0x20);
+          row3 = _mm256_permute2f128_ps(tt3, tt7, 0x20);
+          row4 = _mm256_permute2f128_ps(tt0, tt4, 0x31);
+          row5 = _mm256_permute2f128_ps(tt1, tt5, 0x31);
+          row6 = _mm256_permute2f128_ps(tt2, tt6, 0x31);
+          row7 = _mm256_permute2f128_ps(tt3, tt7, 0x31);
+          // Use unaligned store instructions
+          _mm256_storeu_ps(&dst[jj * n + ii], row0);
+          _mm256_storeu_ps(&dst[(jj + 1) * n + ii], row1);
+          _mm256_storeu_ps(&dst[(jj + 2) * n + ii], row2);
+          _mm256_storeu_ps(&dst[(jj + 3) * n + ii], row3);
+          _mm256_storeu_ps(&dst[(jj + 4) * n + ii], row4);
+          _mm256_storeu_ps(&dst[(jj + 5) * n + ii], row5);
+          _mm256_storeu_ps(&dst[(jj + 6) * n + ii], row6);
+          _mm256_storeu_ps(&dst[(jj + 7) * n + ii], row7);
+        }
+        // Remainder row processing
+        for (int jj = j_max - (j_max % 8); jj < j_max; ++jj)
+          for (int iii = 0; iii < 8 && (ii + iii) < i_max; ++iii)
+            dst[jj * n + (ii + iii)] = src[(ii + iii) * k + jj];
+      }
+      // Balance row processing
+      for (int ii = i_max - (i_max % 8); ii < i_max; ++ii)
+        for (int jj = j; jj < j_max; ++jj)
+          dst[jj * n + ii] = src[ii * k + jj];
+    }
+  }
+}
+*/
+
+// --------------------- MATMUL ------------------------- //
 
 // void matmul_old(const float *A, const float *B, float *C, int n, int k) {
 //   /* Allocate aligned memory for BT for better SIMD performance */
@@ -217,20 +293,7 @@ void matmul_ref(const float *A, const float *B, float *C_ref, int n, int k) {
   }
 }
 
-/*void transpose_old(const float *B, float *BT, int k, int n) {
- * #pragma omp parallel for collapse(2)
- * for (int i = 0; i < k; i += ALIGN_SIZE) {
- *    for (int j = 0; j < n; j += ALIGN_SIZE) {
- *      const int iend = std::min(i + ALIGN_SIZE, k);
- *      const int jend = std::min(j + ALIGN_SIZE, n);
- *      for (int ii = i; ii < iend; ++ii)
- *        for (int jj = j; jj < jend; ++jj)
- *          BT[jj * k + ii] = B[ii * n + jj];
- *    }
- *  }
- *}*/
-
-/* void matmul_ref_s(const float *A, const float *B, float *C_ref, int n, int k)
+/* void matmul_ref(const float *A, const float *B, float *C_ref, int n, int k)
  * {
  *   for (int i = 0; i < n; i++) {
  *     for (int j = 0; j < n; j++) {
